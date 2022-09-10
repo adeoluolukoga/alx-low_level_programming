f08b422c68e507a63c72c7d5a0b915b914a05933
root <olukogaa@yahoo.com>
2022-09-10 12:04:33 -0700

    lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+/* your code goes here*/
+
+{
+char alpha;
+
+alpha = 'a';
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+	putchar(alpha);
+}
+
+	return(0);
+
+}
