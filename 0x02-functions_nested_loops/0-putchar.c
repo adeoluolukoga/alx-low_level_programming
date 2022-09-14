c2e43fb5107a6e9fd6dd7649838fe003d0b09985
root <olukogaa@yahoo.com>
2022-09-13 18:06:51 -0700

    first funstion

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _putchar(char _putchar)
+{
+	char a[] = "_putchar";
+	int b;
+
+	for (b=0: b<8; b++)
+	{
+		_putchar(a[b]);
+	}
+
+		return(0);
+
+}
+
