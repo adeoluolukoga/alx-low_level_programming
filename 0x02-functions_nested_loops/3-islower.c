0370dda8f10abbecfae67eba89f89780ce032767
root <olukogaa@yahoo.com>
2022-09-14 09:59:21 -0700

    alphabet in lowercase test

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c - input letter
+ * Description - it checks if an input is a lowercase
+ * Return 1 if true an 0 if not
+ */
+
+int _islower(int c)
+{
+
+	int c;
+
+	if (_islower(c) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
