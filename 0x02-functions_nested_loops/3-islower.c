35e3c3edecadb28cdac4299a02daf25885cc7832
root <olukogaa@yahoo.com>
2022-09-14 16:05:18 -0700

    islower file updated3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
- * @c - input letter
+ * _islower - identifies lowercase letter
+ * @c: The input letter
+ *
  * Description - it checks if an input is a lowercase
- * Return 1 if true an 0 if not
+ *
+ * Return: 1 if true an 0 if not
  */
 
 int _islower(int c)
-
-/* _islower is function toprint lower case with retun 0 and 1*/
-
 {
+
+/* char c is converted to int r for interpretation on ASCII */
 
 	int r = c;
 
