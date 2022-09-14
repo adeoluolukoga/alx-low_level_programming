401aa1d0ca7e446bef10fb4fce5e4274f28590fb
root <olukogaa@yahoo.com>
2022-09-14 08:44:53 -0700

    -putchar.c file added3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,16 +9,18 @@
 */
 
 int main(void)
+
 {
 	char c[] = "_putchar";
 	int b;
 
-	for (b=0; b<8; b++)
+	for (b = 0; b < 8; b++)
 	{
 		_putchar(c[b]);
 	}
+		_putchar('\n')
 
-		return(0);
+		return (0);
 
 }
 
