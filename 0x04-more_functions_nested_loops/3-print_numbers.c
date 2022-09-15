cbccbc2290700d945f282860d2ff955b0a21d908
root <olukogaa@yahoo.com>
2022-09-15 16:35:03 -0700

    file3 on printing digits created

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digits 0 to 9
+ *
+ * Description - printing is done using putchar
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+		_putchar('\n');
+}
