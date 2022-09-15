717ff8449b20359067b1c00a2d08b75dc8b29f24
root <olukogaa@yahoo.com>
2022-09-15 16:24:58 -0700

    file on multiplication of 2 created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intejers
+ * @a: first input
+ * @b: second input
+ *
+ * Description - it multiplies two intejers and give output
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
