6cd15134af141438003342325cdb75df90b19bb5
root <olukogaa@yahoo.com>
2022-09-15 16:11:04 -0700

    file 1 is digit created

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - identifies digits
+ * @c: The input letter
+ *
+ * Description - it checks if an input is a lowercase
+ *
+ * Return: 1 if true an 0 if not
+ */
+
+int _isupper(int c)
+{
+
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
