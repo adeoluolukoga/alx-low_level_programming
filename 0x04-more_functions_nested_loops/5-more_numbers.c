266b18ebb9f893a47f833de8c707a0b4cb82797f
root <olukogaa@yahoo.com>
2022-09-15 17:12:12 -0700

    file3 on printing digits 0 to 14 10x created1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Description - it prints digits 0 to 14 10xtimes
+ * Return void
+ */
+
+void more_numbers(void)
+{
+	char digit = 0;
+	int count = 1;
+
+	while (count <= 10)
+	{
+
+		for (digit = 0; digit <= 14; digit++)
+		{
+			_putchar(digit);
+		}
+
+		count++;
+
+		_putchar('\n');
+/* note that there is no return bcos return value of the function is void*/
+	}
+}
