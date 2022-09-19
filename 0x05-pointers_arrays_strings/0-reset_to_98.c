1cd977cae382e554db4e8f7484f252f45be3b68f
root <olukogaa@yahoo.com>
2022-09-19 07:55:33 -0700

    first file on pointers to 98-1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
-/** 
-* reset_to_98 - updates value of pointer to 98 
-*
-* @*n: this is the pointer
-* 
-*/
+/**
+ * reset_to_98 - updates value of pointer to 98
+ *
+ * @*n: this is the pointer
+ *
+ */
 
 void reset_to_98(int *n)
 {
 int a; /* actual variable declaration*/
-int *n;
 
 n = &a; /*pointing n to address of variable*/
 *n = 98; /* changing value of 'a' using pointer */
