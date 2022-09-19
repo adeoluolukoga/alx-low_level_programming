f8afabdeb734eddaf823d1a35720913b3401f95f
root <olukogaa@yahoo.com>
2022-09-19 10:22:26 -0700

    file on lenght of string pointers created

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of string with results in bytes
+ *
+ * @s: variable of pointer
+ *
+ * Return: l if successful
+ */
+
+int _strlen(char *s)
+{
+
+int l; /* l is lenght of the string */
+char a[]; /* 'a' is the output of *s in byte for string lenght*/
+
+	*s = a[l];
+
+	return (l);
+
+}
+
