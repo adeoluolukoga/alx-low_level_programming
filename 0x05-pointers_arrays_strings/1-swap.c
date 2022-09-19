d3222dfea0bccd241187b54bb3da851923ea1b7b
root <olukogaa@yahoo.com>
2022-09-19 09:26:37 -0700

    file 2 on pointers created

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap value of one variable to another
+ *
+ * @a: variable 1
+ * @b: variable 2
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+
+*a = b;
+*b = a;
+
+}
+
