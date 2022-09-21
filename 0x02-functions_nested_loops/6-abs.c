8c3023790891cd975612e8e6860c1854fb342ffc
root <olukogaa@yahoo.com>
2022-09-21 15:04:21 -0700

    file 6 and 7 created

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - converts a number to absolute value
+ * @int: The input number
+ *
+ * Description - absolute value.
+ *
+ * Return: int, 0 or -int.
+ */
+
+int _abs(int)
+{
+
+	if (int < 0)
+	{
+		return (int * (-1));
+	}
+	else if (int == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (int);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it prints value of last digit
+ * @int: The input number
+ *
+ * Description - absolute value.
+ *
+ * Return: value for last digit
+ */
+
+int print_last_digit(int)
+{
+
+int lastdigit;
+
+	lastdigit = int % 10;
+
+	return (lastdigit);
+
+}
