c9031ccf5c2599c1dc43d2f9717016130f2075cd
root <olukogaa@yahoo.com>
2022-09-21 12:08:50 -0700

    file on printing sizes of type created

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,11 @@
 int main(void)
 {
 
-	printf("Size of\n");
+printf("%s%lu%s\n","Size of a char: ", sizeof(char), " bytes(s)");
+printf("%s%lu%s\n","Size of an int: ", sizeof(int), " bytes(s)");
+printf("%s%lu%s\n","Size of a long int: ", sizeof(long int), " bytes(s)");
+printf("%s%lu%s\n","Size of a long long int: ", sizeof(long long int), " bytes(s)");
+printf("%s%lu%s\n","Size of a float: ", sizeof(float), " bytes(s)");
 
-	return (0);
+return (0);
 }
