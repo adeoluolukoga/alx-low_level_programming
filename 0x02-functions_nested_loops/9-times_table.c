43fcf318fe2c10658acae46b9765d032a2b99f50
root <olukogaa@yahoo.com>
2022-09-22 23:11:00 -0700

    jack bauer updated4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,18 @@ for (a = 0; a <= 9; a++)
 		}
 		else
 		{
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			_putchar((c / 10) + '0'), _putchar((c % 10) + '0');
 		}
-			if (b != 9)
+			if (c < 10 && b != 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(','), _putchar(' '), _putchar(' ');
+			}
+			else if (c >= 10 && b != 9)
+			{
+				_putchar(','), _putchar(' ');
+			}
+			else
+			{
 			}
 	}
 			_putchar('\n');
