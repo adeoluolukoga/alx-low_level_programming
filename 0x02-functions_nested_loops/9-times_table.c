1622fca4f4995a8e6b800c9c549e08fddeedb4ba
root <olukogaa@yahoo.com>
2022-09-22 22:43:38 -0700

    jack bauer updated

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - it prints multiplication table from 0 to 9
+ * Description - Times table
+ */
+void times_table(void)
+{
+int a = 0;
+int b = 0;
+int c;
+for (a = 0; a <= 9; a++)
+{
+	for (b = 0; b <= 9; b++)
+	{
+		c = a * b;
+		if (c < 10)
+		{
+			putchar(c + '0');
+		}
+		else
+		{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+}
