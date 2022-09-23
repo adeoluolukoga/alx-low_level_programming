d6d4fea15fb6a1257d0a8cdb98b04ab6b646a9a9
root <olukogaa@yahoo.com>
2022-09-22 23:29:33 -0700

    addition file created

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - gives the output of sum of two digits
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return - sum
+ */
+int add(int a, int b)
+{
+int a;
+int b;
+int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
